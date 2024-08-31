f92fffc408e1d8c091cedecffaf9966e94466f08
Chandra0804 <chandrasaiteja0804@gmail.com>
2024-08-31 14:23:52 +0530

    deploy2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Home/', // Set this to your repo name
+  base: '/home/', // Set this to your repo name
   build: {
     outDir: 'build',
   },
